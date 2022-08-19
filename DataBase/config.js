1ad5780ba54fab15095a43601ebe8a4ac40fa46d
stichi-developer <estvrak31@gmail.com>
2022-08-18 22:38:48 -0500

    database => config updated

--- DataBase/config.js
+++ DataBase/config.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
     try {
         await mongoose.connect(process.env.MONGODB_CNN, {
-
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
         })
         console.log('Very good connection online')
     } catch (error) {
